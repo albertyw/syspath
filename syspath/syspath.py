771a15997ccd06f6fd08d92ddeb42a55b999ed43
Albert Wang <git@albertyw.com>
2017-12-18 08:07:11 -0800

    Fixes and add append_current_path

--- syspath/syspath.py
+++ syspath/syspath.py
@@ -9,7 +9,17 @@ def append_path(path):
 
 def caller_path(index):
     """ Get the caller's file path, by the index of the stack """
-    frame = inspect.stack()[index]
-    module = inspect.getmodule(frame[0])
+    module = None
+    while not module:
+        frame = inspect.stack()[index]
+        module = inspect.getmodule(frame[0])
+        index += 1
     filename = module.__file__
-    return os.path.realpath(filename)
+    path = os.path.dirname(os.path.realpath(filename))
+    return path
+
+
+def append_current_path(index=2):
+    """ Append the caller's path to sys.path """
+    path = caller_path(index)
+    append_path(path)
--- syspath/tests/test_syspath.py
+++ syspath/tests/test_syspath.py
@@ -1,11 +1,29 @@
+import copy
 import os
+import sys
 import unittest
 
 import syspath
 
+
 class TestSysPath(unittest.TestCase):
+    def setUp(self):
+        self.orig_sys_path = copy.deepcopy(sys.path)
+
+    def tearDown(self):
+        sys.path = self.orig_sys_path
+
+    def test_append_path(self):
+        syspath.append_path('asdf')
+        self.assertEqual(sys.path[-1], 'asdf')
+
     def test_caller_path(self):
         path = syspath.caller_path(1)
-        expected = os.path.realpath(__file__)
+        expected = os.path.dirname(os.path.realpath(__file__))
         self.assertEqual(path, expected)
 
+    def test_append_current_path(self):
+        syspath.append_current_path()
+        self.assertEqual(len(sys.path), len(self.orig_sys_path)+1)
+        appended_path = sys.path[-1]
+        self.assertEqual(os.path.split(appended_path)[1], 'tests')
