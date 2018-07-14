13f75091a49c46689f1aa957304d6df0d46559cf
Albert Wang <git@albertyw.com>
2018-07-13 23:08:17 -0700

    Make shortcut modules provide a path

--- syspath/current_path.py
+++ syspath/current_path.py
@@ -1,8 +1,7 @@
 from syspath import append_current_path
 
 
-append_current_path(index=4)
+path = append_current_path(index=4)
 
 if __name__ == "__main__":
-    import sys
-    print(sys.path[-1])
+    print(path)
--- syspath/git_root.py
+++ syspath/git_root.py
@@ -1,8 +1,7 @@
 from syspath import append_git_root
 
 
-append_git_root(index=5)
+path = append_git_root(index=5)
 
 if __name__ == "__main__":
-    import sys
-    print(sys.path[-1])
+    print(path)
