6929bf07191fe53e916b02900e33d910660b8a01
Albert Wang <git@albertyw.com>
2020-10-14 21:46:21 -0700

    Fix imports to prevent no-implicit-reexport mypy type issues

--- syspath/__init__.py
+++ syspath/__init__.py
@@ -1,2 +1,6 @@
-from .syspath import *  # noqa: disable=F403
-from .syspath import _append_path, _caller_path  # noqa: disable=F401
+from .syspath import get_current_path as get_current_path  # noqa: disable=F403
+from .syspath import append_current_path as append_current_path  # noqa: disable=F403
+from .syspath import get_git_root as get_git_root  # noqa: disable=F403
+from .syspath import append_git_root as append_git_root  # noqa: disable=F403
+from .syspath import get_parent_path as get_parent_path  # noqa: disable=F403
+from .syspath import append_parent_path as append_parent_path  # noqa: disable=F403
