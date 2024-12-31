9af1cbdd4826104e31edf89c121d3e8b88dbd88c
Albert Wang <git@albertyw.com>
2024-12-31 00:51:18 -0800

    Remove unneeded ruff ignores

--- syspath/__init__.py
+++ syspath/__init__.py
@@ -1,7 +1,7 @@
 # ruff: noqa: PLC0414 https://github.com/astral-sh/ruff/issues/6294
-from .syspath import get_current_path as get_current_path  # noqa: disable=F403
-from .syspath import append_current_path as append_current_path  # noqa: disable=F403
-from .syspath import get_git_root as get_git_root  # noqa: disable=F403
-from .syspath import append_git_root as append_git_root  # noqa: disable=F403
-from .syspath import get_parent_path as get_parent_path  # noqa: disable=F403
-from .syspath import append_parent_path as append_parent_path  # noqa: disable=F403
+from .syspath import get_current_path as get_current_path
+from .syspath import append_current_path as append_current_path
+from .syspath import get_git_root as get_git_root
+from .syspath import append_git_root as append_git_root
+from .syspath import get_parent_path as get_parent_path
+from .syspath import append_parent_path as append_parent_path
