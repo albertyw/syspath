efee82ffa42cf02c1c8d09275b21e2267538e349
Albert Wang <git@albertyw.com>
2024-12-31 00:50:17 -0800

    Ignore PLC0414 because the imports do have semantic meaning

--- syspath/__init__.py
+++ syspath/__init__.py
@@ -1,3 +1,4 @@
+# ruff: noqa: PLC0414 https://github.com/astral-sh/ruff/issues/6294
 from .syspath import get_current_path as get_current_path  # noqa: disable=F403
 from .syspath import append_current_path as append_current_path  # noqa: disable=F403
 from .syspath import get_git_root as get_git_root  # noqa: disable=F403
